be5b03deacfe6d0137552874ba51270f75c36149
wesleyjiang <jiang.yuto@husky.neu.edu>
2018-06-10 20:28:24 -0400

    added naviagtor component and services

---
+++ src/app/course-navigator/course-navigator.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {CourseNavigatorServiceClient} from '../../services/course-navigator.service.client';
+
+
+@Component({
+  selector: 'app-course-navigator',
+  templateUrl: './course-navigator.component.html',
+  styleUrls: ['./course-navigator.component.css']
+})
+export class CourseNavigatorComponent implements OnInit {
+  courses = [];
+  modules = [];
+  lessons = [];
+  widgets = [];
+  selectedCourseId = 0;
+  selectedModuleId = 0;
+  selectedLessonId = 0;
+  constructor(private service: CourseNavigatorServiceClient) { }
+
+  selectCourse(courseId) {
+    this.selectedCourseId = courseId;
+    this.service.findModulesForCourse(courseId)
+      .then(modules =>
+        this.modules = modules);
+  }
+  selectModule(moduleId) {
+    this.selectedModuleId = moduleId;
+    this.service.findLessonsForModule(moduleId)
+      .then(lessons => {
+        this.lessons = lessons;
+      });
+  }
+  selectLesson(lessonId) {
+    this.selectedLessonId = lessonId;
+    this.service.findWidgetsForLesson(lessonId)
+      .then(widgets => {
+        this.widgets = widgets;
+      });
+  }
+
+
+
+  ngOnInit() {
+    this.service.findAllCourses()
+      .then(courses => {
+        this.courses = courses;
+      });
+  }
+
+}
