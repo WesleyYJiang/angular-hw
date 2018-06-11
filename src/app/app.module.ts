17dbbc6531d07c8734f21aeb638ff349137c72b7
wesleyjiang <jiang.yuto@husky.neu.edu>
2018-06-10 20:35:21 -0400

    created individual services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 import { HelloWorldTwoComponent } from './hello-world-two/hello-world-two.component';
 import { WhiteboardComponent } from './whiteboard/whiteboard.component';
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
-import {CourseNavigatorServiceClient} from '../services/course-navigator.service.client';
+import {CourseNavigatorServiceClient} from './services/course-navigator.service.client';
 
 @NgModule({
   declarations: [
