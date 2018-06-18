438befece511754947b8968b0e4e3ae829c39ca1
wesleyjiang <jiang.yuto@husky.neu.edu>
2018-06-18 17:05:27 -0400

    added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 import { ModuleListComponent } from './module-list/module-list.component';
 import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
 import { WidgetListComponent } from './widget-list/widget-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { WidgetListComponent } from './widget-list/widget-list.component';
     CourseViewerComponent,
     ModuleListComponent,
     LessonTabsComponent,
-    WidgetListComponent
+    WidgetListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
